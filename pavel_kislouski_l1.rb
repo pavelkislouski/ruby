a4d990d352189a711ed7949d67088cf19be60426
Pavel Kislouski <pavel_kislouski@epam.com>
2018-04-25 16:33:29 +0300

    task1.1

--- pavel_kislouski_l1.rb
+++ pavel_kislouski_l1.rb
@@ -8,7 +8,20 @@ def task1(input)
     if strs[i].include? 'error'
       task1_result << 'URGENCY!'
       task1_result << strs[i]
-    else
+      i += 1
+    end
+    break if i == strs.size - 1
+  end
+  task1_result
+end
+
+def task2(input)
+  # split output on string
+  task2_result = []
+  strs = input.split(/\n/)
+  i = 0
+  loop do
+    unless strs[i].include? 'error'
       target_string = ''
       date = strs[i][/[0-9]{2}\/[a-zA-Z]+\/[0-9]{4}:\d{2}:\d{2}:\d{2} \+\d{4}/]
       target_string += date
@@ -19,49 +32,10 @@ def task1(input)
       to = to.upcase
       target_string += '  TO: '
       target_string += to
-      task1_result << target_string
+      task2_result << target_string
     end
     i += 1
     break if i == strs.size - 1
   end
-  task1_result
-end
-
-# second task
-def task2(input)
-  # split output on string
-  task2_result = []
-  strs = input.split(/\n/)
-  i = 0
-  massive = []
-  count_massive = 0
-
-  loop do
-    if strs[i].include? 'Calling'
-      need_time = strs[i][/(?<=17:)\d{2}:\d{2}\.\d/]
-      massive[count_massive] = need_time
-      count_massive += 1
-    end
-    i += 1
-    break if i == strs.size - 1
-  end
-
-  minute1 = massive[0][/\d{2}/].to_i * 60
-  minute2 = massive[1][/\d{2}/].to_i * 60
-  minute_res = minute2 - minute1
-
-  sec1 = massive[0][/\d{2}\.\d/].to_f
-  sec2 = massive[1][/\d{2}\.\d/].to_f
-  second_res = sec2 - sec1
-
-  # result
-  task2_result << (minute_res + second_res).round(1)
   task2_result
-end
-
-# third task
-def task3(input)
-  summa = 0
-  input.each_char { |c| summa += c.to_i }
-  summa
 end
